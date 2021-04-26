bb1a96bc29b267db0f78342bef86f6d47b6afed7
neit <dt98nguyen@gmail.com>
2021-04-26 22:58:49 +0700

    Create action bar component

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,6 +1,7 @@
 import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
 interface CellLisItemProps {
   cell: Cell;
@@ -14,7 +15,12 @@ const CellListItem: React.FC<CellLisItemProps> = ({ cell }) => {
     child = <TextEditor cell={cell} />;
   }
 
-  return <div>{child}</div>;
+  return (
+    <div>
+      <ActionBar id={cell.id} />
+      {child}
+    </div>
+  );
 };
 
 export default CellListItem;
