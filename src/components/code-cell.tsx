0386f8fc894132858dd76102d308ebd3e7896dc9
neit <dt98nguyen@gmail.com>
2021-05-04 19:03:27 +0700

    Initial bundle

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,6 +16,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 750);
