52e7de5e074ed98fd0514c99126ed5c0207d1a8d
neit <dt98nguyen@gmail.com>
2021-05-05 21:01:22 +0700

    Fix code cell function

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -19,24 +19,32 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = [
-      `
-        const show = (value) => {
-          const root = document.querySelector('#root');
-          if (typeof value === 'object') {
-            if (value.$$typeof && value.props) {
-              ReactDOM.render(value, root);
-            } else {
-              root.innerHTML = JSON.stringify(value);
-            }
+
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+      var show = (value) => {
+        const root = document.querySelector('#root');
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
           } else {
-            root.innerHTML = value;
+            root.innerHTML = JSON.stringify(value);
           }
+        } else {
+          root.innerHTML = value;
         }
-      `,
-    ];
+      }
+    `;
+    const showFuncNoop = 'var show = () => {}';
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
