320fa7a253fe8495e37f56c9d4837867b9c4655b
neit <dt98nguyen@gmail.com>
2021-05-05 22:13:40 +0700

    create node local api file

---
+++ local-api.js
@@ -0,0 +1,10 @@
+// import express
+
+// one route
+// serve up react production assets
+
+// save a list of cells into a file
+
+// load up a list of cells from a file
+
+// express server listen on port 4050
