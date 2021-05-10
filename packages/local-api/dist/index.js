9e8db70c5a721ff83509a69fd08686bcdca24280
neit <dt98nguyen@gmail.com>
2021-05-10 22:27:19 +0700

    Creating the CLI

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function () {
-    console.log('server is listening');
-});
+exports.serve = void 0;
+var serve = function (port, filename, dir) {
+    console.log('serving traffic on port', port);
+    console.log('saving/fetching cells from', filename);
+    console.log('that file is in dir', dir);
+};
+exports.serve = serve;
