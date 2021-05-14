22739446950d3615baa96d0e1113104339de3281
neit <dt98nguyen@gmail.com>
2021-05-15 01:44:37 +0700

    Integrate google map api

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,43 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  public googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMaker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+
+  // addCompanyMaker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng,
+  //     },
+  //   });
+  // }
+}
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello guys');
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const user1 = new User();
+const company1 = new Company();
+const customMap = new CustomMap('map');
+customMap.addMaker(user1);
+customMap.addMaker(company1);
